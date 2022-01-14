a31d9b6e11e78dce644924de97124db6e76b0c51
DerianCordobaPerez <derianunanleon@gmail.com>
2022-01-14 14:26:54 -0600

    feat(Root): Add cors and common headers for api

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import Express from 'express';
 import productRoutes from './routes/product.routes';
+import cors from 'cors';
+import headers from './middlewares/headers';
 import './config/dotenv.config';
 import './database';
 
@@ -7,7 +9,8 @@ const app = Express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(Express.json());
+app.use(cors());
+app.use(headers);
 
 app.use(productRoutes);
 
