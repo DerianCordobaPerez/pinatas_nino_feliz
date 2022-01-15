370c796d8715bdcbbcec2e22bb57f25c2f352d50
DerianCordobaPerez <derianunanleon@gmail.com>
2022-01-15 14:48:58 -0600

    refactor(Root): Remove useUser hook by ProductFeed component

--- client/pages/product/index.js
+++ client/pages/product/index.js
@@ -1,16 +1,10 @@
-import { useProducts } from 'hooks/useProducts'
 import Layout from 'components/layout'
-import Loading from 'components/loading'
-import ProductList from 'components/products/productList'
+import ProductFeed from 'components/products/productFeed'
 
 export default function Products () {
-  const { products, isLoading } = useProducts()
-
-  if (isLoading) return <Loading />
-
   return (
     <Layout title='Productos'>
-      <ProductList products={products} />
+      <ProductFeed />
     </Layout>
   )
 }
