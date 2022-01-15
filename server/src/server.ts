ec120f676755179c877ba8dc7fd7553bd8290404
DerianCordobaPerez <derianunanleon@gmail.com>
2022-01-15 16:09:00 -0600

    feat(Root): Add handlebars settings

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,35 @@
+import { join } from 'path';
 import Express from 'express';
-import productRoutes from './routes/product.routes';
 import cors from 'cors';
+import { engine } from 'express-handlebars';
+import productRoutes from './routes/product.routes';
 import headers from './middlewares/headers';
 import './config/dotenv.config';
 import './database';
 
+// Create express server
 const app = Express();
 
+// Settings
 app.set('port', process.env.PORT || 3000);
+app.set('views', join(__dirname, 'views'));
+app.engine(
+  '.hbs',
+  engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'), 'layouts'),
+    partialsDir: join(app.get('views'), 'partials'),
+    extname: '.hbs',
+  }),
+);
+app.set('view engine', '.hbs');
 
+// Middlewares
 app.use(cors());
 app.use(headers);
+app.use(Express.static(join(__dirname, 'public')));
 
+// Routes
 app.use('/api/ecommerce/v1/', productRoutes);
 
 export default app;
