6eab0de4f8883cd7fccbbd4f3bf2bb735b1f33a3
DerianCordobaPerez <derianunanleon@gmail.com>
2022-01-25 15:58:55 -0600

    refactor(Root): Change import only types

--- server/src/middlewares/flash.ts
+++ server/src/middlewares/flash.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 
 export const flash = (req: Request, res: Response, next: NextFunction) => {
   res.locals.user = req.user || null;
