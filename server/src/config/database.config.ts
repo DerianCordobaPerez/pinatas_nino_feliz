ce97ec4700f93507b5c658f27147c0cd302265a3
DerianCordobaPerez <derianunanleon@gmail.com>
2022-01-25 19:35:34 -0600

    refactor(Root): Import consts with dynamics values

--- server/src/config/database.config.ts
+++ server/src/config/database.config.ts
@@ -1,9 +1,4 @@
-const DB_HOST = process.env.DB_HOST;
-const DB_NAME = process.env.DB_NAME;
-const DB_USER = process.env.DB_USER;
-const DB_PORT = process.env.DB_PORT;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const NODE_ENV = process.env.NODE_ENV;
+import { NODE_ENV, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } from './env.config';
 
 export const MONGODB_URL =
   NODE_ENV === 'production'
