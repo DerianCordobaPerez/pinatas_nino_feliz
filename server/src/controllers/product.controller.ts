68e56582a022329c74fbca0b581d466b4f810ba4
DerianCordobaPerez <derianunanleon@gmail.com>
2022-02-01 01:45:40 -0600

    feat(Root): Add and remove actions created

--- server/src/controllers/product.controller.ts
+++ server/src/controllers/product.controller.ts
@@ -1,10 +1,41 @@
 import Product from '../models/product';
 import { Request, Response } from 'express';
 
-export async function create(req: Request, res: Response) {
-  const product = new Product(req.body);
+export async function index(req: Request, res: Response) {
+  const products = await Product.find().lean();
+  res.render('products/index', {
+    title: 'Lista de productos',
+    products,
+  });
+}
+
+export function create(req: Request, res: Response) {
+  res.render('products/create', {
+    title: 'Creación de productos',
+  });
+}
+
+export async function store(req: Request, res: Response) {
+  const { name, price, description, quantity, categories, new: isNew } = req.body;
+  const { files } = req;
+  const slug = `${name.replace(/\s+/g, '-').toLowerCase()}-${Math.floor(Math.random() * 100)}`;
+  const images = (files as any[]).map<any>(({ filename }) => filename);
+
+  const product = new Product({
+    name,
+    slug,
+    description,
+    price,
+    quantity,
+    images,
+    categories,
+    new: isNew,
+  });
+
   await product.save();
-  res.json(product);
+
+  req.flash('success', ['Producto creado correctamente']);
+  res.redirect('/products');
 }
 
 export async function find(req: Request, res: Response) {
@@ -24,10 +55,11 @@ export async function update(req: Request, res: Response) {
   res.json(product);
 }
 
-export async function remove(req: Request, res: Response) {
+export async function destroy(req: Request, res: Response) {
   const { slug } = req.params;
   await Product.findOneAndRemove({ slug });
-  res.json({ message: 'Product removed' });
+  req.flash('success', ['Producto eliminado correctamente']);
+  res.redirect('/products');
 }
 
 export async function findByCategory(req: Request, res: Response) {
--- server/src/routes/product.routes.ts
+++ server/src/routes/product.routes.ts
@@ -1,14 +1,26 @@
 import { Router } from 'express';
-import { create, find, findOne, findByCategory, remove, update } from '../controllers/product.controller';
+import {
+  index,
+  create,
+  store,
+  find,
+  findOne,
+  findByCategory,
+  destroy,
+  update,
+} from '../controllers/product.controller';
 import { isAuthenticated } from '../middlewares/auth';
+import { uploadImages } from '../middlewares/multer';
 
 const router = Router();
 
-router.get('/products', find);
-router.get('/products/:slug', findOne);
-router.get('/products/category/:category', isAuthenticated, findByCategory);
-router.post('/products/create', isAuthenticated, create);
-router.put('/products/:slug', isAuthenticated, update);
-router.delete('/products/:slug', isAuthenticated, remove);
+router.get('/api/ecommerce/v1/products', find);
+router.get('/api/ecommerce/v1/products/:slug', findOne);
+router.get('/api/ecommerce/v1/products/category/:category', findByCategory);
+router.get('/products', index);
+router.get('/products/create', isAuthenticated, create);
+router.post('/products/create', [isAuthenticated, uploadImages('images')], store);
+router.put('/products/:slug/edit', isAuthenticated, update);
+router.delete('/products/:slug/destroy', isAuthenticated, destroy);
 
 export default router;
